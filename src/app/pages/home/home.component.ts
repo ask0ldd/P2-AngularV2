837ab8762de405a236897c9ae04a047415742a61
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-18 19:20:32 +0100

    pie chart : color scheme and fn

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
@@ -11,10 +13,29 @@ export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
   public pieChartsDatas$: Observable<{name : string, value : number} []> = of([])
 
-  constructor(private olympicService: OlympicService) {}
+  colorScheme : Color = {
+    domain:['#956065', '#793d52', '#89a1db', '#9780a1', '#bfe0f1'],
+    group: ScaleType.Linear,
+    selectable: true,
+    name: 'Pie Scheme',
+  }
+
+  constructor(private olympicService: OlympicService, private router : Router, private route : ActivatedRoute,) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics$()
     this.pieChartsDatas$ = this.olympicService.getPieChartDatas$()
+  }
+
+  setLabelFormatting(label : string): string {
+    return `${label}`
+  }
+
+  onSelect(event : EventEmitter<any>){
+    // event obj : {name: 'Italy', value: 96, label: 'Italy'}
+    if(event.name != null) {
+      this.router.navigateByUrl(`countrylinechart/${event.name.toLowerCase()}`) 
+      return
+    }
   }
 }
