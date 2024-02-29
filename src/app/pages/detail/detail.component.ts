e72de96c91170ae863c07f29df9a7ca7aa6ef818
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-29 01:05:19 +0100

    detail component

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subscription, of } from 'rxjs';
+import { Observable, Subscription, of, take } from 'rxjs';
 import { ILineChartsDatas } from 'src/app/core/services/interfaces/ILineChartsDatas';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
@@ -12,7 +12,7 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class DetailComponent implements OnInit {
 
   countryName! : string | null
-  linechartDatas$: Observable<ILineChartsDatas> = of()
+  linechartDatas$: Observable<ILineChartsDatas | undefined> = of(undefined)
   YticksList : number[] = [] /* = [0, 5 , 10, 15, 20]*/
   maxMedals! : number
   totalMedals! : number
@@ -21,12 +21,19 @@ export class DetailComponent implements OnInit {
   view : [number, number] = [800, 400]
   totalAthletes$! : Observable<number>
   LineChartDatasSubscription! : Subscription
-  isError : boolean = false 
+
+  isLoadingError$: Observable<boolean> = of(false)
 
   constructor(private olympicService: OlympicService, private router:Router, private route: ActivatedRoute) { }
 
+
+  // !!! deal with http://localhost:4200/detail/3
+
   ngOnInit(): void {
 
+    this.isLoadingError$ = this.olympicService.getLoadingErrorStatus$()
+
+    // if no country in the url
     this.countryName = this.route.snapshot.paramMap.get('id')
     if(this.countryName == null) {
       this.router.navigateByUrl('/404') 
@@ -38,29 +45,34 @@ export class DetailComponent implements OnInit {
     this.LineChartDatasSubscription = this.olympicService.getCountryLineChartDatas$(this.countryName).subscribe({
       
       next : (datas) => {
-        const medalsList = datas.series?.map(serie => serie.value)
-        // retrieve the Y min / max values to scale the Y Axis
-        this.minYaxis = Math.floor((Math.min(...medalsList) / 10)) * 10
-        if(this.minYaxis < 0) this.minYaxis = 0
-        this.maxYaxis = Math.ceil((Math.max(...medalsList) / 10)) * 10
+        // if country doesn't exist in the datas
+        if(datas == null) { 
+          this.router.navigateByUrl('/404') 
+        }
+        else{
+          const medalsList = datas.series?.map(serie => serie.value)
+          // retrieve the Y min / max values to scale the Y Axis
+          this.minYaxis = Math.floor((Math.min(...medalsList) / 10)) * 10
+          if(this.minYaxis < 0) this.minYaxis = 0
+          this.maxYaxis = Math.ceil((Math.max(...medalsList) / 10)) * 10
 
-        this.totalMedals = datas.series.reduce((acc, serie) => acc + serie.value, 0)
+          this.totalMedals = datas.series.reduce((acc, serie) => acc + serie.value, 0)
 
-        // if maxY-minY <= 20 then ticks are spaced by 5
-        // if > 20 then spaced by 10
-        let space = 10
-        if(this.maxYaxis-this.minYaxis <= 20) space = 5
-        if(this.maxYaxis-this.minYaxis <= 10) space = 2
-        let currentTick = this.minYaxis
-        while(currentTick<=this.maxYaxis){
-          this.YticksList.push(currentTick)
-          currentTick += space
+          // if maxY-minY <= 20 then ticks are spaced by 5
+          // if > 20 then spaced by 10
+          let space = 10
+          if(this.maxYaxis-this.minYaxis <= 20) space = 5
+          if(this.maxYaxis-this.minYaxis <= 10) space = 2
+          let currentTick = this.minYaxis
+          while(currentTick<=this.maxYaxis){
+            this.YticksList.push(currentTick)
+            currentTick += space
+          }
         }
       },
 
       error : (error : any) => {
         console.log(error)
-        this.isError = true
       }
     }) // deal with complete ?
 
